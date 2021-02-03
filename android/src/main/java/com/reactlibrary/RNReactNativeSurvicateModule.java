6efa02f990eb03b1bd149af1cc9114c6c4eb83eb
Valery Piniazik <valerypiniazik@Valerys-MacBook-Pro.local>
2021-02-03 19:42:43 +0300

    refactored build gradle

--- android/src/main/java/com/reactlibrary/RNReactNativeSurvicateModule.java
+++ android/src/main/java/com/reactlibrary/RNReactNativeSurvicateModule.java
@@ -5,8 +5,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
-import com.survicate.surveys.Survicate;
-import com.survicate.surveys.traits.UserTrait;
+// import com.survicate.surveys.Survicate;
+// import com.survicate.surveys.traits.UserTrait;
 
 public class RNReactNativeSurvicateModule extends ReactContextBaseJavaModule {
 
@@ -17,43 +17,43 @@ public class RNReactNativeSurvicateModule extends ReactContextBaseJavaModule {
     this.reactContext = reactContext;
   }
 
-  @Override
-  public String getName() {
-    return "RNReactNativeSurvicate";
-  }
+  // @Override
+  // public String getName() {
+  //   return "RNReactNativeSurvicate";
+  // }
 
-  @ReactMethod
-  public void enterScreen(String screenName) {
-      Survicate.enterScreen(screenName);
-  }
+  // @ReactMethod
+  // public void enterScreen(String screenName) {
+  //     Survicate.enterScreen(screenName);
+  // }
 
-  @ReactMethod
-  public void leaveScreen(String screenName) {
-      Survicate.leaveScreen(screenName);
-    }
+  // @ReactMethod
+  // public void leaveScreen(String screenName) {
+  //     Survicate.leaveScreen(screenName);
+  //   }
 
-    @ReactMethod
-  public void invokeEvent(String eventName) {
-      Survicate.invokeEvent(eventName);
-  }
+  //   @ReactMethod
+  // public void invokeEvent(String eventName) {
+  //     Survicate.invokeEvent(eventName);
+  // }
 
-  @ReactMethod
-  public void setUserId(String userId) {
-      Survicate.setUserTrait(new UserTrait.UserId(userId));
-  }
+  // @ReactMethod
+  // public void setUserId(String userId) {
+  //     Survicate.setUserTrait(new UserTrait.UserId(userId));
+  // }
 
-  @ReactMethod
-  public void setUserTrait(String userTrait, String value) {
-      Survicate.setUserTrait(new UserTrait(userTrait, value));
-  }
+  // @ReactMethod
+  // public void setUserTrait(String userTrait, String value) {
+  //     Survicate.setUserTrait(new UserTrait(userTrait, value));
+  // }
 
-  @ReactMethod
-  public void initialize() {
-      Survicate.init(reactContext);
-  }
+  // @ReactMethod
+  // public void initialize() {
+  //     Survicate.init(reactContext);
+  // }
 
-  @ReactMethod
-  public void reset() {
-      Survicate.reset();
-  }
+  // @ReactMethod
+  // public void reset() {
+  //     Survicate.reset();
+  // }
 }
