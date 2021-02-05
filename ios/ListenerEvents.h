2ea85ee473ad9bab849bb0915abe21a6a976f002
Valery Piniazik <valerypiniazik@Valerys-MacBook-Pro.local>
2021-02-05 12:40:23 +0300

    added listener events

---
+++ ios/ListenerEvents.h
@@ -0,0 +1,8 @@
+#ifndef ListenerEvents_h
+#define ListenerEvents_h
+@import SurvicateDelegate;
+
+@interface ListenerEvents () <SurvicateDelegate>
+@end
+
+#endif
---
+++ ios/ListenerEvents.m
@@ -0,0 +1,22 @@
+#import "ListenerEvents.h"
+@import SurvicateAnswer;
+
+@implementation AppDelegate
+
+- (void)questionAnsweredWithSurveyId:(NSString * _Nonnull)surveyId questionId:(NSInteger)questionId answer:(SurvicateAnswer * _Nonnull)answer {
+
+}
+
+- (void)surveyClosedWithSurveyId:(NSString * _Nonnull)surveyId {
+
+}
+
+- (void)surveyCompletedWithSurveyId:(NSString * _Nonnull)surveyId {
+
+}
+
+- (void)surveyDisplayedWithSurveyId:(NSString * _Nonnull)surveyId {
+
+}
+
+@end
